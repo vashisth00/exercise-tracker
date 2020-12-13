c25dd43b833f35ec1e80544188c9aaeae2a77d55
Vashisth00 <vashisthbhushan@gmail.com>
2020-12-13 18:41:00 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { BrowserRouter as Router, Route} from "react-router-dom";
+
+import Navbar from "./components/navbar"
+import ExercisesList from "./components/exercises-list";
+import EditExercise from "./components/edit-exercise";
+import CreateExercise from "./components/create-exercise";
+import CreateUser from "./components/create-user";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className="container">
+    <Navbar />
+    <br/>
+    <Route path="/" exact component={ExercisesList} />
+    <Route path="/edit/:id" component={EditExercise} />
+    <Route path="/create" component={CreateExercise} />
+    <Route path="/user" component={CreateUser} />
     </div>
+  </Router>
   );
 }
 
